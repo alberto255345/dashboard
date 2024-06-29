3d1ee2e7a17a135ee046a6d27cedfa9b7c3a7af5
Alberto Vitoriano <alberto.vitoriano@ciadoleite.com.br>
2024-06-28 22:40:43 -0300

    fix: correção

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import { useState, useRef } from 'react';
+import { useState, useRef, useCallback } from 'react';
 import Grid from '@mui/material/Grid';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import axios from 'axios';
+import axios, { AxiosRequestConfig, RawAxiosRequestHeaders } from 'axios';
+import { useLongPress } from 'use-long-press';
 
 interface ButtonUsageProps {
   style: React.CSSProperties; // Definindo o tipo para CSSProperties
@@ -11,79 +12,75 @@ interface ButtonUsageProps {
 
 const ButtonUsage: React.FC<ButtonUsageProps> = ({ style }) => {
   const [progress, setProgress] = useState(0);
+  const [enabled, setEnabled] = useState(true);
+  const [longPressed, setLongPressed] = useState(false);
   const progressBarContainerRef = useRef<HTMLDivElement>(null);
   const holdTime = 3000; // 3 seconds
-  let timer: ReturnType<typeof setTimeout>;
-  let interval: ReturnType<typeof setInterval>;
-  let startTime: number;
+  const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
+  const startTimeRef = useRef<number>(0);
 
-  const startTimer = (event: React.MouseEvent | React.TouchEvent) => {
-    event.preventDefault();
-    startTime = Date.now();
-    setProgress(0);
-    if (progressBarContainerRef.current) {
-      progressBarContainerRef.current.style.display = 'block';
-    }
+  const callback = useCallback(() => {
+    setLongPressed(true);
+    sendGetRequest();
+  }, []);
 
-    timer = setTimeout(() => {
-      sendGetRequest();
-      if (progressBarContainerRef.current) {
-        progressBarContainerRef.current.style.display = 'none';
-      }
-      setProgress(0); // Reset progress to 0 after request is sent
-    }, holdTime);
-
-    interval = setInterval(updateProgressBar, 50); // Update every 50ms
-  };
-
-  const cancelTimer = () => {
-    clearTimeout(timer);
-    clearInterval(interval);
-    setProgress(0);
-    if (progressBarContainerRef.current) {
-      progressBarContainerRef.current.style.display = 'none';
-    }
-  };
+  const bind = useLongPress(enabled ? callback : null, {
+    onStart: (event, meta) => {
+      console.log("Press started", meta);
+      startTimeRef.current = Date.now();
+      const id = setInterval(updateProgressBar, 50); // Update every 50ms
+      setIntervalId(id);
+    },
+    onFinish: (event, meta) => {
+      setLongPressed(false);
+      console.log("Long press finished", meta);
+      if (intervalId) clearInterval(intervalId);
+      setProgress(0);
+    },
+    onCancel: (event, meta) => {
+      console.log("Press cancelled", meta);
+      if (intervalId) clearInterval(intervalId);
+      setProgress(0);
+    },
+    threshold: holdTime,
+  });
 
   const updateProgressBar = () => {
-    const elapsedTime = Date.now() - startTime;
+    const elapsedTime = Date.now() - startTimeRef.current;
     const progressValue = Math.min((elapsedTime / holdTime) * 100, 100);
     setProgress(progressValue);
   };
 
-  const sendGetRequest = () => {
-    const apiUrl = import.meta.env.REACT_APP_API_URL || 'https://example.com/default-url';
-    axios.get(apiUrl, {
+  const sendGetRequest = async () => {
+    console.log('Enviando requisição GET...');
+    const apiUrl = import.meta.env.REACT_APP_API_URL || 'http://ip.jsontest.com/';
+
+    const config: AxiosRequestConfig = {
       headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*', // Adicione este header
+        'Accept': 'application/json',
+      } as RawAxiosRequestHeaders,
+    };
+
+    try {
+      const response = await axios.get(apiUrl, config);
+      if (response.status === 200) {
+        toast.success('Sucesso!');
+      } else {
+        toast.error('Erro.');
       }
-    })
-    .then(response => {
-      console.log('Success:', response.data);
-      toast.success('Get request successful!');
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-      toast.error('Get request failed.');
-    });
+    } catch (error) {
+      toast.error('Erro ao enviar requisição.');
+    }
   };
 
   return (
     <main id="app" style={style}>
       <Grid item>
-        <button
-          onMouseDown={startTimer}
-          onTouchStart={startTimer}
-          onMouseUp={cancelTimer}
-          onMouseLeave={cancelTimer}
-          onTouchEnd={cancelTimer}
-          onTouchCancel={cancelTimer}
-        >
+        <button {...bind()}>
           <span className="text">Abrir Portão</span>
           <span className="shimmer"></span>
         </button>
-        <div id="progressBarContainer" ref={progressBarContainerRef} style={{ display: 'none', width: '100%', backgroundColor: '#e0e0e0', height: '30px', marginTop: '10px' }}>
+        <div id="progressBarContainer" ref={progressBarContainerRef} style={{ display: progress > 0 ? 'block' : 'none', width: '100%', backgroundColor: '#e0e0e0', height: '30px', marginTop: '10px' }}>
           <div id="progressBar" style={{ width: `${progress}%`, height: '100%', backgroundColor: '#76c7c0' }}></div>
         </div>
       </Grid>
